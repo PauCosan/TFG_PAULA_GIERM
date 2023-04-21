df98f8bd98c7168296053c5a2f8ad139b5fb77b1
Paula Cosano <pclalge@gmail.com>
2023-04-21 15:40:39 +0200

    Cambio modelo CNN- Añadir los resultados de cada etapa

--- TFG1.1.py
+++ TFG1.1.py
@@ -1,3 +1,4 @@
+
 import codecs
 import csv
 import numpy as np
@@ -6,14 +7,66 @@ from tensorflow import keras
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from keras.preprocessing.text import Tokenizer
-from keras.layers import Embedding, SimpleRNN, Dense, Dropout, LSTM
+from keras.layers import Embedding, Conv1D, MaxPooling1D, Flatten, Dense, Dropout
 from keras.models import Sequential
 from keras.optimizers import Adam
 
 # Cargar los datos
-with codecs.open('1-classification-trainset.tsv', 'r', encoding='utf-8') as tsvfile:
+with codecs.open('1-classification-testset.tsv', 'r', encoding='utf-8') as tsvfile:
     lector_tsv = csv.reader(tsvfile, delimiter='\t')
     tweets = [fila for fila in lector_tsv]
+
+tweets = pd.DataFrame(tweets[1:], columns=tweets[0])
+
+# Obtener tweets y etiquetas
+tweets_text = tweets['text'].values
+labels = tweets['label'].values
+
+print(tweets['label'])
+
+# Obtener el número de etiquetas totales
+num_etiquetas = len(labels)
+print("Número de etiquetas totales:", num_etiquetas)
+
+# Obtener el número de etiquetas que tienen un valor de cero
+num_etiquetas_cero = np.count_nonzero(labels == '0')
+print("Número de etiquetas con valor cero:", num_etiquetas_cero)
+
+# Obtener el número de etiquetas que tienen un valor de uno
+num_etiquetas_uno = np.count_nonzero(labels == '1')
+print("Número de etiquetas con valor uno:", num_etiquetas_uno)
+
+
+
+
+"""
+# Cargar los datos
+with codecs.open('AccidentsTweets.tsv', 'r', encoding='utf-8') as tsvfile:
+    lector_tsv = csv.reader(tsvfile, delimiter='\t')
+    tweets = [fila for fila in lector_tsv]
+
+# Convertir a DataFrame
+tweets = pd.DataFrame({'text': [fila[14] for fila in tweets[1:]],
+                       'label': [fila[23] for fila in tweets[1:]]})
+
+# Obtener tweets y etiquetas
+tweets_text = tweets['text'].values
+labels = tweets['label'].values
+
+# Obtener el número de etiquetas totales
+num_etiquetas = len(labels)
+print("Número de etiquetas totales:", num_etiquetas)
+
+# Obtener el número de etiquetas que tienen un valor de cero
+num_etiquetas_cero = np.count_nonzero(labels == '0')
+print("Número de etiquetas con valor cero:", num_etiquetas_cero)
+
+# Obtener el número de etiquetas que tienen un valor de uno
+num_etiquetas_uno = np.count_nonzero(labels == '1')
+print("Número de etiquetas con valor uno:", num_etiquetas_uno)
+
+
+---------------------------
 
 # Convertir a DataFrame
 tweets = pd.DataFrame(tweets[1:], columns=tweets[0])
@@ -21,6 +74,23 @@ tweets = pd.DataFrame(tweets[1:], columns=tweets[0])
 # Obtener tweets y etiquetas
 tweets_text = tweets['text'].values
 labels = tweets['label'].values
+
+
+# Obtener el número de etiquetas totales
+num_etiquetas = len(labels)
+print("Número de etiquetas totales:", num_etiquetas)
+
+# Obtener el número de etiquetas que tienen un valor de cero
+num_etiquetas_cero = np.count_nonzero(labels == '0')
+print("Número de etiquetas con valor cero:", num_etiquetas_cero)
+
+# Obtener el número de etiquetas que tienen un valor de uno
+num_etiquetas_uno = np.count_nonzero(labels == '1')
+print("Número de etiquetas con valor uno:", num_etiquetas_uno)
+
+-------------------
+
+
 
 # Convertir las etiquetas a un arreglo numpy
 labels = np.array(labels)
@@ -40,27 +110,27 @@ le.fit(y_train)
 y_train = le.transform(y_train)
 y_test = le.transform(y_test)
 
-# Crear la arquitectura del modelo RNN
+# Crear la arquitectura del modelo CNN
 embedding_dim = 100
 vocab_size = len(tokenizer.word_index) + 1
 model = Sequential()
 model.add(Embedding(vocab_size, embedding_dim, input_length=100))
-model.add(LSTM(128, return_sequences=True))
-model.add(Dropout(0.5))
-model.add(LSTM(64))
-model.add(Dropout(0.5))
+model.add(Conv1D(64, 5, activation='relu'))
+model.add(MaxPooling1D(pool_size=4))
+model.add(Flatten())
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(1, activation='sigmoid'))
 
 
 # Compilar el modelo
-optimizer = Adam(learning_rate=0.001)
-model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+model.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.001), metrics=['accuracy'])
 
 # Entrenar el modelo
-model.fit(X_train, y_train, epochs=10, batch_size=64)
+model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_test, y_test))
 
 # Evaluar el modelo en el conjunto de prueba
 score = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy: %.2f%%" % (score[1]*100))
+
+"""
