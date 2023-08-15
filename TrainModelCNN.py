2ffaf9543e5127f9732720bbb725605b179f9adb
Paula Cosano <pclalge@gmail.com>
2023-08-15 15:29:16 +0200

    final

--- TrainModelCNN.py
+++ TrainModelCNN.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from keras.preprocessing.text import Tokenizer
 from tensorflow import keras
-from keras.layers import Dense, Embedding, Conv1D, GlobalMaxPooling1D
+from keras.layers import Dense, Embedding, Conv1D, GlobalMaxPooling1D, Dropout
 from keras.models import Sequential
 
 # Cargar los datos para train
@@ -101,6 +101,8 @@ embedding_vecor_length = 32
 model = Sequential()
 model.add(Embedding(top_words, embedding_vecor_length, input_length=max_review_length))
 model.add(Conv1D(64, 5, activation='relu'))
+model.add(Dropout(0.5)) #p2
+model.add(Conv1D(128, 3, activation='relu')) #p2
 model.add(GlobalMaxPooling1D())
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
@@ -109,7 +111,6 @@ print(model.summary())
 
 # Train the model
 model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=3, batch_size=64)
-#model.fit(X_train, y_train, epochs=3, batch_size=64) #-> sin añadir los datos de validacion
 
 # Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
--- TrainModelRNN.py
+++ TrainModelRNN.py
@@ -1,6 +1,5 @@
 import codecs
 import csv
-import pickle
 import re
 import numpy as np
 import pandas as pd
@@ -8,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from keras.preprocessing.text import Tokenizer
 from tensorflow import keras
-from keras.layers import Dense, LSTM, Embedding
+from keras.layers import Dense, LSTM, Embedding, Dropout
 from keras.models import Sequential
 
 # Cargar los datos para train
@@ -101,19 +100,22 @@ max_review_length = 100
 embedding_vecor_length = 32
 model = Sequential()
 model.add(Embedding(top_words, embedding_vecor_length, input_length=max_review_length))
-model.add(LSTM(100))
+#model.add(LSTM(100))
+model.add(LSTM(100, return_sequences=True)) #p2
+model.add(Dense(64, activation='relu'))#p2
+model.add(Dropout(0.5)) #p2
+model.add(LSTM(50)) #p2
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
 
 # Train the model
 model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=3, batch_size=64)
-#model.fit(X_train, y_train, epochs=3, batch_size=64) #-> sin añadir los datos de validacion
 
 # Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
 
-model.save('ModelNeuralNetworkRNN.h5')
+model.save('ModelNeuralNetworkRNN2.h5')
 
 
